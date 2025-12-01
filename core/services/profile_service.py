96d98abcc0c4e81bd7dea58473da9c5e039ae856
Egor Buslov <buslovegor5@gmail.com>
2025-12-01 21:01:29 +1000

    update profile

--- core/services/profile_service.py
+++ core/services/profile_service.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.repositories.profile_repo import ProfileRepository
+from core.schemas.profile_schema import ProfileSchema, ProfileUpdateSchema
 
 
 class ProfileService:
@@ -12,7 +13,17 @@ class ProfileService:
         self.session = session
 
         self.profile_repo = ProfileRepository(session=self.session)
-        
-        
+
     async def get_my_profile(self, eid: int):
         return await self.profile_repo.get_profile(eid=eid)
+
+    async def update_my_profile(
+        self, eid: int, profile_data: ProfileUpdateSchema
+    ) -> ProfileSchema:
+
+        updated_profile = await self.profile_repo.update_profile(
+            eid=eid, profile_data=profile_data
+        )
+
+        await self.session.commit()
+        return ProfileSchema.model_validate(updated_profile)
