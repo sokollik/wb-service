b5a15aba882a868b25eea2cf7f4a64fcf7f7e2e3
pukita <pukita@MacBook-Pro-pukita.local>
2025-12-03 18:34:48 +1000

    add common repo

---
+++ core/utils/common_util.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from fastapi import HTTPException
+from src.common.common_exc import IntervalServerErrorHttpException
+
+
+
+def exception_handler(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        try:
+            return await func(*args, **kwargs)
+        except Exception as e:
+            if isinstance(e, HTTPException):
+                raise e
+
+            raise IntervalServerErrorHttpException(msg=str(e))
+
+    return wrapper
